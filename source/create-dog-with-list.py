f1d0197e26226a47aa0576ed2090f8215e5d9ec2
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-12-06 11:06:37 -0500

    feat: Add the create-dog-with-list.

---
+++ source/create-dog-with-list.py
@@ -0,0 +1,17 @@
+# display a message indicating the purpose of this script
+print("Creating dog objects using a list-based approach!")
+
+# TODO: define the characteristics of a dog using a list
+# TODO: refer to the slides for the specific values to pick
+
+print()
+
+# TODO: display the characteristics of the dog named Bosco
+# TODO: please add one print statement for each index of bosco
+
+print()
+
+# TODO: display the characteristics of the dog named Faith
+# TODO: please add one print statement for each index of faith
+
+print()
